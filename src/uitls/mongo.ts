179efaa0ef995a580890b0edcf2bd6fae253a47e
AlexanderRoth98 <alexander.roth1998@gmail.com>
2025-02-28 15:55:29 +0100

    add cluster to env var

--- src/uitls/mongo.ts
+++ src/uitls/mongo.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@albumchart.rtql1.mongodb.net/?retryWrites=true&w=majority&appName=AlbumChart`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/?retryWrites=true&w=majority&appName=AlbumChart`;
 
 let client = new MongoClient(uri);
 let clientPromise: Promise<MongoClient>;
