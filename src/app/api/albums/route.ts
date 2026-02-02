7848c804bb4fd143954652c9ef0b1b020ed14b2a
AlexanderRoth98 <alexander.roth1998@gmail.com>
2026-02-02 13:20:22 +0100

    fix: assure consistent order of albums

--- src/app/api/albums/route.ts
+++ src/app/api/albums/route.ts
@@ -7,9 +7,9 @@ export async function GET() {
   try {
     const client = await clientPromise;
     const db = client.db("albums");
-    const largeAlbums = await db.collection("large_albums").find({}).toArray();
-    const mediumAlbums = await db.collection("medium_albums").find({}).toArray();
-    const smallAlbums = await db.collection("small_albums").find({}).toArray();
+    const largeAlbums = await db.collection("large_albums").find({}).sort({ _id: 1 }).toArray();
+    const mediumAlbums = await db.collection("medium_albums").find({}).sort({ _id: 1 }).toArray();
+    const smallAlbums = await db.collection("small_albums").find({}).sort({ _id: 1 }).toArray();
     return NextResponse.json({ largeAlbums, mediumAlbums, smallAlbums });
   } catch (e) {
     console.error(e);
